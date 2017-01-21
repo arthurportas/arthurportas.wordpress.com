47f53b3430734f7d910d141f3a20c167679eae50
Arthur Portas <arthurportas@gmail.com>
2017-01-21 22:31:52 +0000

    add test class rule for mongodb in-memory

--- nosql/mongo/fongo-hello-world/src/test/java/com/arthurportas/unit/repository/UserRepositoryTest.java
+++ nosql/mongo/fongo-hello-world/src/test/java/com/arthurportas/unit/repository/UserRepositoryTest.java
@@ -3,7 +3,9 @@ package com.arthurportas.unit.repository;
 import com.arthurportas.mongodb.example.repository.UserRepository;
 import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
 import com.lordofthejars.nosqlunit.core.LoadStrategyEnum;
+import com.lordofthejars.nosqlunit.mongodb.InMemoryMongoDb;
 import com.lordofthejars.nosqlunit.mongodb.MongoDbRule;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static com.lordofthejars.nosqlunit.mongodb.InMemoryMongoDb.InMemoryMongoRuleBuilder.newInMemoryMongoDbRule;
 import static com.lordofthejars.nosqlunit.mongodb.MongoDbRule.MongoDbRuleBuilder.newMongoDbRule;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
@@ -26,6 +29,9 @@ public class UserRepositoryTest {
 
     private static Logger LOGGER = LoggerFactory.getLogger(UserRepositoryTest.class);
 
+    @ClassRule
+    public static InMemoryMongoDb inMemoryMongoDb = newInMemoryMongoDbRule().build();
+    
     @Rule
     public MongoDbRule mongoDbRule = newMongoDbRule().defaultSpringMongoDb("demo-test");
 
